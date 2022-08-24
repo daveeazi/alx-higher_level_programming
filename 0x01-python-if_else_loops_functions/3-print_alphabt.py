653c614725f6c7d674e6f1aee5ca4ab8a9cd6356
David <davidatat8@gmail.com>
2022-08-24 15:55:51 +0100

    Fixed the program that prints the ASCII alphabet, in lowercase, not followed by a new line

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-        if i != 101 and i != 113:
-                    print("{:c}".format(i), end='')
+
+for ch in range(97, 123):
+    if ch == 101 or ch == 113:
+        continue
+    print("{:c}".format(ch), end="")
