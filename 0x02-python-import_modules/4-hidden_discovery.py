e2ddb388bb1d80ec248fd43b787dcc7f59060941
David Atat <davidatat8@gmail.com>
2022-08-26 06:34:03 -0700

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-import hidden_4
+
 if __name__ == "__main__":
-    for name in dir(hidden_4):
-        if "__" not in name:
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
             print(name)
