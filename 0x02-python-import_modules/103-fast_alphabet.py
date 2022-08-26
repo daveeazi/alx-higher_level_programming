4d2059f34fa6a7ba7ddba59adfb93c7f237062b7
David <davidatat8@gmail.com>
2022-08-26 13:29:18 +0100

    prints the alphabet in uppercase

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,18 +1,3 @@
 #!/usr/bin/python3
-
-def lowercaseAlphabets():
-
-    for c in range(97, 123):
-        print(chr(c), end = " ");
-
-    print("");
-
-def uppercaseAlphabets():
-
-    for c in range(65, 91):
-        print(chr(c), end = " ");
-
-    print("");
-
-uppercaseAlphabets();
-lowercaseAlphabets();
+import string
+print(string.ascii_uppercase)
