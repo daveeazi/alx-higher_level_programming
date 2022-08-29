b08b5e1c1605fd2c988e12f908dd6c50375c772d
David <davidatat8@gmail.com>
2022-08-29 16:27:28 +0100

    fixed bug

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    counts = my_string.count('c')
-    new_string = list(my_string)
-    while counts:
-        new_string.remove('c')
-        counts -= 1
-    counts = my_string.count('C')
-    while counts:
-        new_string.remove('C')
-        counts -= 1
-    new_string = ''.join(new_string)
-    return 
+    newStr = ""
+    for letter in my_string:
+        if (letter != 'c' and letter != 'C'):
+            newStr += letter
+    return (newStr)
