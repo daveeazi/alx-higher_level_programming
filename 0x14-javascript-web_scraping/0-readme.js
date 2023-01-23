cf2d83394630d0e12f514cc85ddade95867b3531
David <davidatat8@gmail.com>
2023-01-24 00:30:58 +0100

    Fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
-/**
-  reads and prints the content of a file
-  first argument is the file path
- */
-  const myArgs = process.argv.slice(2);
-  const fs = require('fs');
-  fs.readFile(myArgs[0], 'utf8', (error, data) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(data);
-    }
-  });
+// A script that reads and prints the content of a file
+//
+// const file = process.argv[2];
+// const fs = require('fs');
+//
+// fs.readFile(file, 'utf8', (error, data) => {
+//   if (error) {
+//       console.log(error);
+//         } else {
+//             console.log(data);
+//               }
+//               });
